34c01d35492b5334875236f47ea89967e9bc8fcf
thiagohbn <thiagohenri19@gmail.com>
2022-06-13 09:15:30 -0300

    Primeiro Commit

---
+++ src/main/java/dev/camila/unitTesting/JUnit5/App.java
@@ -0,0 +1,10 @@
+package dev.camila.unitTesting.JUnit5;
+
+public class App {
+    
+    public static void main(String[] args) {
+    	System.getProperties().list(System.out);
+    }
+    
+    
+}
